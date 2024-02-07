9452eca783213e7eb67936777ffbfd4be4dc5f4b
patelsilky <103938029+patelsilky@users.noreply.github.com>
2024-02-06 23:20:59 -0500

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -1,15 +1,36 @@
-# Assignment2_mypet
-using System;
+        {
+            Console.WriteLine("\nMain Menu:");
+            Console.WriteLine($"1. Feed {pet.Name}");
+            Console.WriteLine($"2. Play with {pet.Name}");
+            Console.WriteLine($"3. Let {pet.Name} Rest");
+            Console.WriteLine($"4. Check {pet.Name}'s status");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("User Input: ");
+            int choice = int.Parse(Console.ReadLine());
 
-public static class Menu
-{
-    public static void DisplayMainMenu(Pet pet)
-    {
-        Console.WriteLine($"Main menu for {pet.Name}:");
-        Console.WriteLine("1. Feed");
-        Console.WriteLine("2. Play");
-        Console.WriteLine("3. Rest");
-        Console.WriteLine("4. Check status");
-        Console.WriteLine("5. Exit");
+            switch (choice)
+            {
+                case 1:
+                    pet.Feed();
+                    break;
+                case 2:
+                    pet.Play();
+                    break;
+                case 3:
+                    pet.Rest();
+                    break;
+                case 4:
+                    pet.Status();
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
